160b1af560793b8cdb253dc945739309d8da62eb
ec3594 <echen5877@gmail.com>
2022-12-14 20:18:22 -0500

    fixed a line exceeding max length bug

--- server/controllers/Character.js
+++ server/controllers/Character.js
@@ -4,77 +4,77 @@ const CharacterModel = require('../models/Character');
 const { Character } = models;
 
 const makerPage = (req, res) => {
-    res.render('app');
+  res.render('app');
 };
 
 const upgradedPage = (req, res) => {
-    res.render('app2');
+  res.render('app2');
 };
 
-const makeCharacter = async(req, res) => {
-    if (!req.body.name || !req.body.age || !req.body.characteristic) {
-        return res.status(400).json({ error: 'All fields are required!' });
-    }
+const makeCharacter = async (req, res) => {
+  if (!req.body.name || !req.body.age || !req.body.characteristic) {
+    return res.status(400).json({ error: 'All fields are required!' });
+  }
 
-    const characterData = {
-        name: req.body.name,
-        age: req.body.age,
-        characteristic: req.body.characteristic,
-        owner: req.session.account._id,
-    };
+  const characterData = {
+    name: req.body.name,
+    age: req.body.age,
+    characteristic: req.body.characteristic,
+    owner: req.session.account._id,
+  };
 
-    try {
-        const newCharacter = new Character(characterData);
-        await newCharacter.save();
-        return res.status(201).json({
-            name: newCharacter.name,
-            age: newCharacter.age,
-            characteristic: newCharacter.characteristic,
-        });
-    } catch (err) {
-        console.log(err);
-        if (err.code === 11000) {
-            return res.status(400).json({ error: 'Character already exists!' });
-        }
-        return res.status(400).json({ error: 'An error occured' });
+  try {
+    const newCharacter = new Character(characterData);
+    await newCharacter.save();
+    return res.status(201).json({
+      name: newCharacter.name,
+      age: newCharacter.age,
+      characteristic: newCharacter.characteristic,
+    });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Character already exists!' });
     }
+    return res.status(400).json({ error: 'An error occured' });
+  }
 };
 
-const getCharacters = (req, res) => CharacterModel.findByOwner(req.session.account._id, (err, docs) => {
-    if (err) {
-        console.log(err);
-        return res.status(400).json({ error: 'An error occurred!' });
-    }
+const getChars = (req, res) => CharacterModel.findByOwner(req.session.account._id, (err, docs) => {
+  if (err) {
+    console.log(err);
+    return res.status(400).json({ error: 'An error occurred!' });
+  }
 
-    return res.json({ characters: docs });
+  return res.json({ characters: docs });
 });
 
-const deleteCharacter = async(req, res) => {
-    console.log('in deleteCharacter');
-    // console.log(req.query);
-    console.log(req.query._id);
-    const id = req.body.characterId;
-    console.log(`id: ${id}`);
+const deleteCharacter = async (req, res) => {
+  console.log('in deleteCharacter');
+  // console.log(req.query);
+  console.log(req.query._id);
+  const id = req.body.characterId;
+  console.log(`id: ${id}`);
 
-    try {
-        Character.findOneAndDelete({ name: 'a' });
-        // const doc = await Character.findByIdAndDelete({ id });
-        //   return res.status(202).json({
-        //     name: newCharacter.name,
-        // });
-        return res.status(202);
-    } catch (err) {
-        console.log(err);
-        if (err.code === 11000) {
-            return res.status(400).json({ error: 'Character already exists!' });
-        }
-        return res.status(400).json({ error: 'An error occured' });
+  try {
+    Character.findOneAndDelete({ name: 'a' });
+    // const doc = await Character.findByIdAndDelete({ id });
+    //   return res.status(202).json({
+    //     name: newCharacter.name,
+    // });
+    return res.status(202);
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Character already exists!' });
     }
+    return res.status(400).json({ error: 'An error occured' });
+  }
 };
 module.exports = {
-    makerPage,
-    upgradedPage,
-    makeCharacter,
-    getCharacters,
-    deleteCharacter,
+  makerPage,
+  upgradedPage,
+  makeCharacter,
+  getChars,
+  deleteCharacter,
 };
--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ const mid = require('./middleware');
 
 const router = (app) => {
     app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
-    app.get('/getCharacters', mid.requiresLogin, controllers.Character.getCharacters);
+    app.get('/getChars', mid.requiresLogin, controllers.Character.getChars);
 
     app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
     app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
